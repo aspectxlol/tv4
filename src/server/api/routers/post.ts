756849796a1adaae919618751ac1fbc686ac352a
Louie Hansen <gamernxt6@gmail.com>
2024-01-28 22:22:42 +0700

    fix?

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -16,14 +16,21 @@ export const postRouter = createTRPCRouter({
     }),
 
   create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
+    .input(z.object({
+      title: z.string().min(1),
+      description: z.string().min(1),
+      image: z.string().min(1)
+
+    }))
     .mutation(async ({ ctx, input }) => {
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
       return ctx.db.post.create({
         data: {
-          name: input.name,
+          title: input.title,
+          description: input.description,
+          image: input.image,
           createdBy: { connect: { id: ctx.session.user.id } },
         },
       });
